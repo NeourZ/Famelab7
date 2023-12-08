32ec6ba41582c72dbfc39969b5e965d1fe1a854d
Fame <pattanitfame@gmail.com>
2023-12-09 01:06:17 +0700

    lab7_3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if (x == 'A'){
+		return 'Z' ;
+	}
+	else{
+		if(x > 'Z'){
+			return '0' ;
+		}
+		else{
+			if(x < 'B'){
+				return '0' ;
+			}
+			else{
+				return x-1 ;
+			}
+		}
+	}
 }
 
 int main(){
